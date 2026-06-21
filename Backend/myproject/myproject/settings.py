ed48833a9c7a6f370af40c4078c3b5cf0f73c1e9
fidhapk47-source <fidhapk47@gmail.com>
2026-06-22 00:52:36 +0530

    update requirements

--- Backend/myproject/myproject/settings.py
+++ Backend/myproject/myproject/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-ijy-u))q#h2*7d5jjm6j_wo5n)#y(8k+cwl%8$g8fy&xvgn0#-'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ["*"]
 
